26bd47a7accf76e5b02d4b9423e418183077a2e8
Ibrahim Saad Ibrahim Ali <abrahimsaad271@gmail.com>
2021-05-29 22:04:54 +0200

    Commit from vs code

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.use(appModules)
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.y700p.mongodb.net/notesAppDB', { useNewUrlParser: true, useUnifiedTopology: true })
 
-<<<<<<< HEAD
-// app.listen(port, () => console.log("The Server is Up and Running!!"))
-=======
+
 app.listen(process.env.app.PORT, () => console.log("The Server is Up and Running!!"))
->>>>>>> 0ebac6c1682eca21fbc4de85d56c20ea4c544245
+
